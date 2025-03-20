aa84f20e0dc3b496a42a79c722cb7170cb042c17
M-Raka-S <suryakusumamuhammadraka@gmail.com>
2025-03-20 08:54:03 +0700

    Make parameter value nullable, removes irrelevant database seeder logic

--- database/migrations/2_create_parameters_table.php
+++ database/migrations/2_create_parameters_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
 
         Schema::create('instance_parameters', function (Blueprint $table) {
             $table->id();
-            $table->string('value');
+            $table->string('value')->nullable();
             $table->unsignedBigInteger('parameter_id')->index();
             $table->unsignedBigInteger('instance_id')->index();
             $table->timestamps();
